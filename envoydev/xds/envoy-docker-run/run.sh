c844c4f082a6dd8e8edf5a31048eb6f86d1dbc33
lijiaocn <lijiaocn@foxmail.com>
2019-08-16 20:11:18 +0800

    m

--- envoydev/xds/envoy-docker-run/run.sh
+++ envoydev/xds/envoy-docker-run/run.sh
@@ -1,2 +1,14 @@
 IMAGE=envoyproxy/envoy:v1.11.0
-docker run -idt --network=host -v `pwd`/envoy-0-example.yaml:/etc/envoy/envoy.yaml -v `pwd`/log:/var/log/envoy $IMAGE
+
+if  [ $# -ne 1 ];then
+	echo "must choose one config file"
+	exit 1
+fi
+
+config=$1
+
+if [ `uname`="Darwin" ];then
+	docker run -idt -p 9901:9901 -p 80-86:80-86 -v `pwd`/$config:/etc/envoy/envoy.yaml -v `pwd`/log:/var/log/envoy $IMAGE
+else
+	docker run -idt --network=host -v `pwd`/$config:/etc/envoy/envoy.yaml -v `pwd`/log:/var/log/envoy $IMAGE
+fi
