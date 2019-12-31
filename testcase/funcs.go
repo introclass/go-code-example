7705f8555ebebd69c51a03e00f087a614d6df821
lijiaocn <lijiaocn@foxmail.com>
2020-01-01 00:16:03 +0800

    testcase example

---
+++ testcase/funcs.go
@@ -0,0 +1,17 @@
+package testcase
+
+func SumSlice(nums []int) int {
+	sum := 0
+	for _,v := range nums{
+		sum=sum+v
+	}
+	return sum
+}
+
+func SumArray(nums [10]int) int{
+	sum := 0
+	for _,v := range nums{
+		sum=sum+v
+	}
+	return sum
+}
---
+++ testcase/funcs_test.go
@@ -0,0 +1,29 @@
+package testcase
+
+import (
+	"testing"
+)
+
+func TestSumSlice(t *testing.T) {
+	arrays := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	expect := 45
+	sum := SumSlice(arrays[:])
+	if sum != expect {
+		t.Errorf("result is %d (should be %d)", sum, expect)
+	}
+}
+
+func BenchmarkSumSlice(b *testing.B) {
+	arrays := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s := arrays[:]
+	for i := 0; i < b.N; i++ {
+		SumSlice(s)
+	}
+}
+
+func BenchmarkSumArray(b *testing.B) {
+	arrays := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for i := 0; i < b.N; i++ {
+		SumArray(arrays)
+	}
+}
